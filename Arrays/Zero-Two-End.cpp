2d796b12da50b85fe0ed98c74379d9126e3b9e9c
HarshMaurya1 <harshly977@gmail.com>
2025-12-27 13:55:23 +0530

    Added array problem: Move all zeros to end

---
+++ Arrays/Zero-Two-End.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter The Size Of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter Your Elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    int a = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] != 0)
+        {
+            nums[a] = nums[i];
+            a++;
+        }
+    }
+    for (int i = a; i < n; i++)
+    {
+        nums[i] = 0;
+    }
+    cout << "Result Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+        }
+    return 0;
+}
