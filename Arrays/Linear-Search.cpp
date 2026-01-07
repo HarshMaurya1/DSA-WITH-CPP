6b20556cce0607c5c5ffec2e3ecb8fa40d09cb9e
HarshMaurya1 <harshly977@gmail.com>
2026-01-07 16:04:57 +0530

    enter a number and get a ouput of indexing value enter your target number

---
+++ Arrays/Linear-Search.cpp
@@ -0,0 +1,31 @@
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "Enter The Size Of array: ";
+//     cin >> n;
+
+//     vector<int> nums(n);
+//     cout << "Enter Your Elements: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> nums[i];
+//     }
+//     int target;
+//     cout << "Enter Target  Value: ";
+//     cin >> target;
+
+//     int index = -1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (nums[i] == target)
+//         {
+//             index = i;
+//             break;
+//         }
+//     }
+//     cout << "Your Value in Array Have this Indexing: " << index << endl;
+// }
