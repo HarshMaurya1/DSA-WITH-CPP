c4fe672f9a10a8ee34e8122b54b851568a312a51
HarshMaurya1 <harshly977@gmail.com>
2026-01-20 18:28:34 +0530

    get a output for smallest digit in array

---
+++ Arrays/Small-Digi.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+// #include <cstring>
+// #include <limits.h>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[] = {1, 3, 5, 7, 9, 10, 15, 25, 95};
+//     int size = 9;
+//     int small = INT_MAX;
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (arr[i] < small)
+//         {
+//             // found a number greater than maxi' so update maxi>>
+//             small = arr[i];
+//         }
+//     }
+//     cout << "Greater Number is: " << small << endl;
+// }
