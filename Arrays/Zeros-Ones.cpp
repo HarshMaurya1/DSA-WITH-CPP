b4bec15a0633ad04de898c5368bff6da637df67e
HarshMaurya1 <harshly977@gmail.com>
2026-01-23 01:36:36 +0530

    One of the Interesting Question we find how many zero's in array and One's

---
+++ Arrays/Zeros-Ones.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+// int main()
+// {
+//     int arr[15] = {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1};
+
+//     int size = 15;
+//     int num_Zero = 0;
+//     int num_One = 0;
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (arr[i] == 0)
+//         {
+//             num_Zero++;
+//         }
+//         if (arr[i] == 1)
+//         {
+//             num_One++;
+//         }
+//     }
+//     cout << "Number of Zeros: " << num_Zero << endl;
+//     cout << "Number of Ones: " << num_One << endl;
+// }
